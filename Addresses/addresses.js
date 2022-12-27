a22b833971be041f70ed397b87d5f6dd91da3749
hamzaakrammogul <101792969+Hamzaakrammogul@users.noreply.github.com>
2022-12-27 14:38:01 +0500

    Updated ABI and Address for NFT contract

--- Addresses/addresses.js
+++ Addresses/addresses.js
@@ -1,4 +1,4 @@
-const createNFTContract = "0xcC70DF57Ee4449FecE1511929D537b1A4a4b2d66";
+const createNFTContract = "0xd5b728Bda26791A5AA229818c6FE466072683C2c";
 const createCubeContract= "0xD9fe723aa3e5a55593F412Cf047Df28BEBA00386";
 const auctionsContract= "0xfa6e9B14D493f33E1232Ee416A9f9a371f8e1175";
 const wethContract= "0x9921CDd0d8819F22b4B53B1cb700cF0d7f8B7149";
